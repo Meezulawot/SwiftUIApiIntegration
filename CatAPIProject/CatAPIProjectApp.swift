330f80d6cbdb42487ee36d5ca83f7a42c852a9e9
Meezu Lawot <meezu.lawot@swifttech.com.np>
2023-10-09 11:11:44 +0545

    Initial Commit

---
+++ CatAPIProject/CatAPIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIProjectApp.swift
+//  CatAPIProject
+//
+//  Created by Meezu Lawot on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
